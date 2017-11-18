5587df7e2154974e2090a138227e2e7bfeea9aa9
tn51108 <khwanchanok.sr@tnis.com>
2017-11-18 15:26:54 +0700

    IncreaseBalance update test case

--- internal/middleware/increaseBalance.go
+++ internal/middleware/increaseBalance.go
@@ -1,10 +1,11 @@
 package middleware
 
 type merchant struct {
-	Wallid string
-	Balance float32
+	Wallid string `json:"wallid"`
+	Balance float32 `json:"balance"`
 }
 
 func (m *merchant)Increase(bal float32) {
 	m.Balance+=bal
 }
+
--- internal/middleware/increaseBalance_test.go
+++ internal/middleware/increaseBalance_test.go
@@ -2,17 +2,6 @@ package middleware
 
 import "testing"
 
-func TestDisplayMerchantBalanceBeforeIncrease1000(t *testing.T) {
-	m := merchant{
-		Wallid:  "20001",
-		Balance: 1000,
-	}
-	if m.Balance != 1000 {
-		t.Error("Balance != 1000")
-	}
-
-}
-
 func TestDisplayMerchantBalanceAfterIncrease1000(t *testing.T) {
 	m := merchant{
 		Wallid:  "20001",
