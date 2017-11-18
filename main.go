cfbd764759fb9680494b6d6c96b304ba62fd0a9d
jiraphonSa <jiraphon.sa@tnis.com>
2017-11-18 14:35:49 +0700

    Add libary 2

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
-package TN_Wallet
+package main
+
+import (
+	"log"
+	"net/http"
+	"mux"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+
+	//router.HandleFunc("/helloworld", getHelloworld).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+
+}
+
+
+
+
